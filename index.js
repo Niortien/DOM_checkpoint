96954327820a31c3e146418bcde75d71a824eed0
Niortien <niortiencoulibaly2001@gmail.com>
2023-07-20 14:45:01 +0000

    checkpoint_DOM

--- index.js
+++
@@ -1,7 +0,0 @@
-
-    let btn=document.querySelector(".plus");
-    btn.addEventListener("click",plus);
-    function plus(){
-        let qnty=document.querySelector(".quantité");
-        qnty.value=parseInt(qnty)+1
-    }
